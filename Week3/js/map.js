04cff5375305e2b2125b4e30c108cb0804f0d6dd
Michelle Lee <johndoe@example.com>
2021-04-18 19:58:41 -0700

    update map aesthetics

--- Week3/js/map.js
+++ Week3/js/map.js
@@ -16,7 +16,7 @@ let data = [
     {
         'title':'Boston',
         'time': ': Last travelled here in Summer 2017.',
-        'description': ' I did not realize how many colleges were in the area and how close they were.',
+        'description': 'As we were travelling to our hotel, we came across several blocks of brick houses. It was definitely an unusual sight, coming from California where we are prone to earthquakes.',
         'image': 'images/boston.png',
         'lat': 42.3601,
         'lon': -71.0589
@@ -24,7 +24,7 @@ let data = [
     {
         'title':'New York City',
         'time': ': Last travelled here in Summer 2017.',
-        'description': ' Nothing beats $1 slices of thin crust pizza!',
+        'description': 'Our last destination before boarding the flight home was to a popular pizza place called 2 Bros Pizza. Nothing beats $1 slices of thin crust pizza!',
         'image': 'images/newyork.png',
         'lat': 40.7128,
         'lon': -74.0060
@@ -32,7 +32,7 @@ let data = [
     {
         'title':'Chicago',
         'time': ': Last travelled here in Summer 2018.',
-        'description': ' I really enjoyed walking the Chicago Riverwalk during the evening.',
+        'description': 'We did a lot of walking this day, and I really enjoyed strolling down the Chicago Riverwalk in the evening. On this day, I logged over 26,000 steps!',
         'image': 'images/chicago.JPG',
         'lat': 41.8781,
         'lon': -87.6298
@@ -40,7 +40,7 @@ let data = [
     {
         'title':'Honolulu',
         'time': ': Last travelled here in Summer 2019.',
-        'description': ' It was so lush and beautiful; I really want to go back someday.',
+        'description': 'The mountain in the background is actually the Diamond Head Crater, one of the most famuos hiking spots in Oahu.',
         'image': 'images/hawaii.JPG',
         'lat': 21.3069,
         'lon': -157.8583
@@ -48,7 +48,7 @@ let data = [
     {
         'title':'Los Angeles',
         'time': ': Last travelled here in March 2020.',
-        'description': ' It is crazy that I have not been back to UCLA in more than a year. I hope I will come back soon...',
+        'description': 'It is crazy that I have not been back to UCLA in more than a year. A typical night-time activity to do with friends was to go to Pizookie Tuesdays at BJs. I hope to come back soon...',
         'image': 'images/losangeles.png',
         'lat': 34.0522,
         'lon': -118.2437
@@ -81,3 +81,4 @@ map.fitRounds(myMarkers.getRounds())
 function flyToIndex(index){
     map.flyTo([data[index].lat, data[index].lon],12)
 }
+
